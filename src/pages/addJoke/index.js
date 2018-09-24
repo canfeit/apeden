5b9e4b7e87b4b5bb12541bd7592ccd00f22e7698
canfeit <canfeit@qq.com>
2018-09-24 22:19:34 +0800

    chore

--- src/pages/addJoke/index.js
+++ src/pages/addJoke/index.js
@@ -1,24 +1,24 @@
-import Taro, { Component } from '@tarojs/taro';
-import { View, Text } from '@tarojs/components';
-import { AtForm, AtInput, AtTextarea, AtButton } from 'taro-ui';
-import './index.css';
+import Taro, { Component } from "@tarojs/taro";
+import { View, Text } from "@tarojs/components";
+import { AtForm, AtInput, AtTextarea, AtButton } from "taro-ui";
+import "./index.css";
 const db = wx.cloud.database();
 
 export default class Index extends Component {
   config = {
-    navigationBarTitleText: '写段子',
+    navigationBarTitleText: "写段子"
   };
   constructor(props) {
     super(props);
     this.newJoke = {};
   }
   componentWillMount() {
-    db.collection('jokes').get({
+    db.collection("jokes").get({
       success: res => {
         // 输出 [{ "title": "The Catcher in the Rye", ... }]
         // console.log(res);
         // this.setState({ jokes: res.data });
-      },
+      }
     });
   }
 
@@ -36,21 +36,22 @@ export default class Index extends Component {
     this.newJoke.content = e.target.value;
   }
   onSubmit(e) {
-    db.collection('jokes')
+    console.log(123, this.newJoke);
+    db.collection("jokes")
       .add({
         // data 字段表示需新增的 JSON 数据
-        data: this.newJoke,
+        data: this.newJoke
       })
       .then(res => {
         Taro.navigateTo({
-          url: '/pages/index/index',
+          url: "/pages/index/index"
         });
       });
   }
   render() {
     return (
       <View className="index">
-        <AtForm onSubmit={this.onSubmit.bind(this)} onReset={this.onReset.bind(this)}>
+        <AtForm>
           <AtInput
             name="value1"
             title="标题"
@@ -65,11 +66,8 @@ export default class Index extends Component {
             placeholder="段子内容..."
             onBlur={this.onContent.bind(this)}
           />
-          <AtButton type="primary" size="small" onClick={this.onSubmit} formType="submit">
+          <AtButton type="primary" size="normal" onClick={this.onSubmit}>
             提交
-          </AtButton>
-          <AtButton type="primary" size="small" formType="reset">
-            重置
           </AtButton>
         </AtForm>
       </View>
